df01e226d8678a44315fd4259d03ba2218dd45f8
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-07 12:35:54 -0700

    creating note form component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Container} from 'react-bootstrap'
 import {Routes, Route, Navigate} from "react-router-dom"
+import { NewNote } from "./NewNotes"
 
 function App() {
   return(
     <Container className="my-4">
       <Routes>
               <Route path='/' element={<h1>Home</h1>} />
-              <Route path='/new' element={<h1>New</h1>} />
+              <Route path='/new' element={<NewNote />} />
               <Route path='*' element={<Navigate to='/' />}/>
               <Route path='/:id'>
                 <Route path="index" element={<h1>show</h1>}/>
---
+++ vite-project/src/NoteForm.tsx
@@ -0,0 +1,17 @@
+import {Form, Stack, Row, Col} from 'react-bootstrap';
+
+export function NoteForm(){
+    return(
+        <Form>
+            <Stack gap={4}>
+                <Row>
+                    <Col>
+                        <Form.Group>
+
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Stack>
+        </Form>
+    )
+}
