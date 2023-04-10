2f4b31b9087d168732b06635f3d703e353a30e15
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-10 13:07:21 -0700

    Added create button

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,6 +5,7 @@ import {Routes, Route, Navigate} from "react-router-dom"
 import { NewNote } from "./NewNotes"
 import { useLocalStorage } from "./useLocalStorage"
 import {v4 as uuidV4} from 'uuid'
+import { NoteList } from './NoteList'
 
 
 export type Note = {
@@ -59,7 +60,7 @@ function App() {
   return(
     <Container className="my-4">
       <Routes>
-              <Route path='/' element={<h1>Home</h1>} />
+              <Route path='/' element={<NoteList />} />
               <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} 
               availableTags={tags}
               />} />
