39a6c96b4cb038be54636f1ff461b9e1f01e689d
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-13 13:42:26 -0700

    delete now works

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -55,6 +55,12 @@ function App() {
     })
   }
 
+  function onDeleteNote(id: string){
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   function onUpdateNote(id: string, {tags, ...data}: NoteData) {
     setNotes(prevNotes => {
       return prevNotes.map(note => {
@@ -81,7 +87,7 @@ function App() {
               />} />
               
               <Route path="/:id" element={<NoteLayout notes={notesWithTags}/>}>
-                <Route index element={<Note />}/>
+                <Route index element={<Note onDelete={onDeleteNote}/>}/>
                 <Route path="edit" element= {<EditNote onSubmit={onUpdateNote} onAddTag={addTag} 
               availableTags={tags}/>}/>
               </Route>
